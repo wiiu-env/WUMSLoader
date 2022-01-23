883ecc37910789333771697d72bdfa3b95785b51
Maschell <Maschell@gmx.de>
2022-01-23 22:11:33 +0100

    Move the relocator to not override the EnvrionmentLoader

--- source/globals.h
+++ source/globals.h
@@ -6,10 +6,10 @@
 #define MEMORY_REGION_START         0x00800000
 #define MEMORY_REGION_SIZE          0x00800000
 
-#define MEMORY_REGION_USABLE_HEAP_START  (MEMORY_REGION_START + 0x00080000)
-#define MEMORY_REGION_USABLE_HEAP_END    (MEMORY_REGION_USABLE_HEAP_START + 0x00100000)
+#define MEMORY_REGION_USABLE_HEAP_START  (MEMORY_REGION_START + 0x00080000)                 // We don't want to override the relocator
+#define MEMORY_REGION_USABLE_HEAP_END    (MEMORY_REGION_USABLE_HEAP_START + 0x00080000)     // heap size is 512 KiB for now
 
 #define MEMORY_REGION_USABLE_START  MEMORY_REGION_USABLE_HEAP_END
-#define MEMORY_REGION_USABLE_END    0x00FFF000
+#define MEMORY_REGION_USABLE_END    0x00FFF000                                              // The last 0x1000 bytes are reserved kernel hook
 
 #define gModuleData ((module_information_t *) (MEMORY_REGION_USABLE_START))
