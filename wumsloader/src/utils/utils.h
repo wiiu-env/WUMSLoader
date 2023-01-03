0031dbebbe00371b6fa987b70afcca0fe1714988
Maschell <Maschell@gmx.de>
2023-01-03 22:36:14 +0100

    Make sure the basemodule is one of the first modules

--- wumsloader/src/utils/utils.h
+++ wumsloader/src/utils/utils.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <memory>
+#include <vector>
+
 
 template<class T, class... Args>
 std::unique_ptr<T> make_unique_nothrow(Args &&...args) noexcept(noexcept(T(std::forward<Args>(args)...))) {
@@ -14,4 +16,17 @@ inline typename std::unique_ptr<T> make_unique_nothrow(size_t num) noexcept {
 template<class T, class... Args>
 std::shared_ptr<T> make_shared_nothrow(Args &&...args) noexcept(noexcept(T(std::forward<Args>(args)...))) {
     return std::shared_ptr<T>(new (std::nothrow) T(std::forward<Args>(args)...));
+}
+
+template<typename T, class Allocator, class Predicate>
+bool remove_first_if(std::vector<T, Allocator> &list, Predicate pred) {
+    auto it = list.begin();
+    while (it != list.end()) {
+        if (pred(*it)) {
+            list.erase(it);
+            return true;
+        }
+        it++;
+    }
+    return false;
 }
