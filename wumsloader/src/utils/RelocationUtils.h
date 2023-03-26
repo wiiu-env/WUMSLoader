b5a4f7ca37b1f9e37fddffb3ceeca6173ba81387
Maschell <Maschell@gmx.de>
2023-03-26 16:46:24 +0200

    Remove the skipAllocFunction parameter

--- wumsloader/src/utils/RelocationUtils.h
+++ wumsloader/src/utils/RelocationUtils.h
@@ -12,5 +12,4 @@ bool doRelocation(const std::vector<std::shared_ptr<ModuleData>> &moduleList,
                   const std::vector<std::unique_ptr<RelocationData>> &relocData,
                   relocation_trampoline_entry_t *tramp_data,
                   uint32_t tramp_length,
-                  bool skipAllocReplacement,
                   std::map<std::string, OSDynLoad_Module> &usedRPls);
