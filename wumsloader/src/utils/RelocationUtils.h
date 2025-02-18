199e959dccfe1699e1ac7ea5e846036e56f94e3c
Maschell <Maschell@gmx.de>
2025-02-18 08:18:50 +0100

    Add support for 0.3.4 to allow an custom rpl allocator

--- wumsloader/src/utils/RelocationUtils.h
+++ wumsloader/src/utils/RelocationUtils.h
@@ -4,12 +4,13 @@
 #include <map>
 #include <vector>
 
-bool ResolveRelocations(std::vector<std::shared_ptr<ModuleData>> &loadedModules,
-                        bool skipMemoryMappingModule,
+bool ResolveRelocations(const std::vector<std::shared_ptr<ModuleData>> &loadedModules,
+                        bool skipUnloadedRpl,
                         std::map<std::string, OSDynLoad_Module> &usedRPls);
 
 bool doRelocation(const std::vector<std::shared_ptr<ModuleData>> &moduleList,
                   const std::vector<std::unique_ptr<RelocationData>> &relocData,
                   relocation_trampoline_entry_t *tramp_data,
                   uint32_t tramp_length,
-                  std::map<std::string, OSDynLoad_Module> &usedRPls);
+                  std::map<std::string, OSDynLoad_Module> &usedRPls,
+                  bool skipUnloadedRpl);
